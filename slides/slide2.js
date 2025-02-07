01da855960c1734a6076dd84ea4e9f89dfc83daa
VLDM <connection-vlademyanov@yandex.ru>
2025-02-07 13:02:21 +0300

    Upadate UX

--- slides/slide2.js
+++
@@ -1,7 +0,0 @@
-// slide2.js
-// Экспорт функции, которая выводит содержимое второго слайда
-export function showSlide(terminalElement) {
-    terminalElement.innerHTML += "\n=== Слайд 2 ===\n";
-    terminalElement.innerHTML += "Это второй слайд презентации.\n";
-    terminalElement.innerHTML += "Продолжаем знакомство с материалом.\n\n";
-}
